69605ce077e98e85d49762d56e64c10422368456
Kasperkall <47249916+Kasperkall@users.noreply.github.com>
2021-03-12 16:14:23 +0100

    fixes

--- rail.py
+++ rail.py
@@ -1,19 +1,11 @@
 #Creates a costume rail from the user-inputs adjusting and pasting the templates
 #for lines and curves into the rail template
 
-dfaPath = "C:\\Users\\Amalie\\Documents\\GitHub\\TMM4275-KBE-project\\DFAs\\" #The location of your DFA files
+dfaPath = "K:\\Biblioteker\\Dokumenter\\Skole\\Automatisering\\TMM4275-Assignment2\\DFAs\\" #The location of your DFA files
 
 f = open(dfaPath + "templates\\Rail_template.dfa", "r") 
 rail = f.read()
 f.close() #Opens and reads the DFA template so that a new DFA file of the order can be made
-
-startx = 0#added by user input
-starty = 0
-startz = 0
-
-rail = rail_temp.replace("<STARTX>", str(startx)) #First we need to insert the start position for our rail
-rail = rail.replace("<STARTY>", str(starty))
-rail = rail.replace("<STARTZ>", str(startz))
 
 f = open(dfaPath + "templates\\Arc_template.dfa", "r") 
 arc_temp = f.read()
@@ -23,9 +15,23 @@ f = open(dfaPath + "templates\\Line_template.dfa", "r")
 line_temp = f.read()
 f.close() #Opens and reads the line DFA template so that straight lines can be added to the rail
 
+f = open(dfaPath + "templates\\path_and_combine.dfa", "r") 
+the_end = f.read()
+f.close() #Opens and reads the DFA file that contains the the joining and coloring
 
-def add_arc(masse, sjuke,inputs):
-    new_arc = arc_temp.copy()
+startx = 0#added by user input
+starty = 0
+startz = 0
+
+rail = rail.replace("Rail_template (ug_base_part)", "Rail_Order (ug_base_part)") #Replaces the template with the customers chair values
+rail = rail.replace("<STARTX>", str(startx)) #First we need to insert the start position for our rail
+rail = rail.replace("<STARTY>", str(starty))
+rail = rail.replace("<STARTZ>", str(startz))
+
+
+
+def add_arc(index,radius,angle1,angle2,x,y,z,string):
+    new_arc = arc_temp
     new_arc = new_arc.replace("<INDEX>", str(index))
     new_arc = new_arc.replace("<RADIUS>", str(radius))
     new_arc = new_arc.replace("<ANGLE1>", str(angle1))
@@ -34,10 +40,11 @@ def add_arc(masse, sjuke,inputs):
     new_arc = new_arc.replace("<YCENTER>", str(y))
     new_arc = new_arc.replace("<ZCENTER>", str(z))
 
-    rail = rail + new_arc
+    string = string + new_arc
+    return string
 
-def add_line(masse, sjuke,inputs):
-    new_line = line_temp.copy()
+def add_line(index,x1,y1,z1,x2,y2,z2,string):
+    new_line = line_temp
     new_line = new_line.replace("<INDEX>", str(index))
     new_line = new_line.replace("<X1>", str(x1))
     new_line = new_line.replace("<X2>", str(x2))
@@ -46,5 +53,14 @@ def add_line(masse, sjuke,inputs):
     new_line = new_line.replace("<Z1>", str(z1))
     new_line = new_line.replace("<Z2>", str(z2))
 
-    rail = rail + new_line
+    string = string + new_line
+    return string
 
+rail = add_line(1,0,0,0,0,10000,0,rail)
+rail = rail + the_end
+
+rail = rail.replace("<ALL_ELEMENTS>", "line_1:")
+
+f = open(dfaPath + "Rail_Order.dfa", "w") #Saves the customers chair as a new DFA file with the name My_Chair_Order.dfa
+f.write(rail)
+f.close()
