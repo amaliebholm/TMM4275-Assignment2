59eb352b241a77674079c11084efbcd685880c81
Amalie Berge Holm <eaholm@stud.ntnu.no>
2021-03-18 15:10:48 +0100

    trying grid

--- DFA_RailServer.py
+++ DFA_RailServer.py
@@ -56,50 +56,13 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes('<br>Room hight:<br><input type="text" name="room_hight" value="0">', "utf-8"))
             s.wfile.write(bytes('<br>Room length:<br><input type="text" name="room_length" value="0">', "utf-8"))
             s.wfile.write(bytes('<br>Room width:<br><input type="text" name="room_width" value="0">', "utf-8"))
-            s.wfile.write(bytes('<br><br><input type="submit" value="Render"></form><p> Click "Render" to make the preview grid</p>', "utf-8"))
-            
+            s.wfile.write(bytes('<br><br> <button id = "renderButton"> Render </button> </form><p> Click "Render" to make the preview grid</p>', "utf-8"))
+
             s.wfile.write(bytes('<h4>Add variales in the room:</h4>', "utf-8"))
 
-            s.wfile.write(bytes('<style>.button {border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 1px 2px; transition-duration: 0.4s; cursor: pointer;}', "utf-8"))
-            #s.wfile.write(bytes('<script> changeColor(button, color){count = 1; property = document.getElementById(button); if(count==0){property.style.backgroundColor = "white"}; count = 1;} else{property.style.backgroundColor = color; count = 0;}}</script>', "utf-8"))
-            #s.wfile.write(bytes('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>', "utf-8"))
-            #s.wfile.write(bytes('<script> $(() => {"use strict"; $("button").click(function() {$(this).toggleClass("pressed");});})</script>', "utf-8"))
-
-            s.wfile.write(bytes('.button1 {background-color: white; color: black; border: 2px solid #008000;} ', "utf-8"))
-            s.wfile.write(bytes('.button1:hover {background-color: #008000; color: white;} ', "utf-8"))
-            #s.wfile.write(bytes('.button1.pressed {background-color: #008000; color: white;} ', "utf-8"))
-
-            s.wfile.write(bytes('.button2 {background-color: white; color: black; border: 2px solid #FF0000;} ', "utf-8"))
-            s.wfile.write(bytes('.button2:hover {background-color: #FF0000; color: white;} ', "utf-8"))
-
-            s.wfile.write(bytes('.button3 {background-color: white; color: black; border: 2px solid #000000; } ', "utf-8"))
-            s.wfile.write(bytes('.button3:hover {background-color: #000000; color: white;} ', "utf-8"))
-
-            s.wfile.write(bytes('.button4 {background-color: white; color: black; border: 2px solid #0000FF;} ', "utf-8"))
-            s.wfile.write(bytes('.button4:hover {background-color: #0000FF; color: white;} </style>', "utf-8"))
-
-            s.wfile.write(bytes('<button class="button button1">Click here to add an attachment point for the rails in the ceiling </button>', "utf-8"))
-            s.wfile.write(bytes('<button class="button button2">Click here to add locations the cart should visit </button>', "utf-8"))
-            s.wfile.write(bytes('<button class="button button3">Click here to add an obstacle in the room, the feeder can not pass through these points </button>', "utf-8"))
-            s.wfile.write(bytes('<button class="button button4">Click here the feeding location for the cart </button>', "utf-8"))
-
-            s.wfile.write(bytes('<h4>Grid of the room:</h4>', "utf-8"))
-            s.wfile.write(bytes('<head><style> table {width:50%;} table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;} </style></head>', "utf-8"))
-            # 1x1 meter cells in the grid
-            table = ""
-            cols = ""
-            for w in range(room_width):
-                cols += "<td></td>"
-
-            for l in range(room_length):
-                table += "<tr>" + cols + "</tr>"
-            s.wfile.write(bytes('<table>' + table + '</table>', "utf-8"))
-            s.wfile.write(bytes('<br><br><input type="submit" value="Submit"></form><p> Click "Submit" to send order.</p></body></html>', "utf-8"))
         else:
-            s.wfile.write(
-                bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
-            s.wfile.write(
-                bytes("<body><p>The path: " + path + "</p>", "utf-8"))
+            s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
+            s.wfile.write(bytes("<body><p>The path: " + path + "</p>", "utf-8"))
             s.wfile.write(bytes('</body></html>', "utf-8"))
 
     def do_POST(s):
@@ -136,53 +99,56 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes('<br>Room hight:<br><input type="text" name="room_height" value="' + str(room_height) + '">', "utf-8"))
             s.wfile.write(bytes('<br>Room length:<br><input type="text" name="room_length" value="' + str(room_length) + '">', "utf-8"))
             s.wfile.write(bytes('<br>Room width:<br><input type="text" name="room_width" value="' + str(room_width) + '">', "utf-8"))
-            s.wfile.write(bytes('<br><br><input type="submit" value="Render"></form><p> Click "Render" to make the preview grid</p>', "utf-8"))
-            
-            s.wfile.write(bytes('<h4>Add variales in the room:</h4>', "utf-8"))
-            s.wfile.write(bytes('<style>.button {border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 1px 2px; transition-duration: 0.4s; cursor: pointer;}', "utf-8"))
-            #s.wfile.write(bytes('<script> changeColor(button, color){count = 1; property = document.getElementById(button); if(count==0){property.style.backgroundColor = "white"}; count = 1;} else{property.style.backgroundColor = color; count = 0;}}</script>', "utf-8"))
-            #s.wfile.write(bytes('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>', "utf-8"))
-            #s.wfile.write(bytes('<script> $(() => {"use strict"; $("button").click(function() {$(this).toggleClass("pressed");});})</script>', "utf-8"))
+            s.wfile.write(bytes('<br><br> <button id="renderButton"> Render </button> </form><p> Click "Render" to make the preview grid</p>', "utf-8"))
 
+            s.wfile.write(bytes('<h4>Add variales in the room:</h4>', "utf-8"))
+            
+            s.wfile.write(bytes('<style>.button {border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 1px 2px; transition-duration: 0.4s; cursor: pointer;}', "utf-8"))
             s.wfile.write(bytes('.button1 {background-color: white; color: black; border: 2px solid #008000;} ', "utf-8"))
             s.wfile.write(bytes('.button1:hover {background-color: #008000; color: white;} ', "utf-8"))
-            #s.wfile.write(bytes('.button1.pressed {background-color: #008000; color: white;} ', "utf-8"))
+           
 
             s.wfile.write(bytes('.button2 {background-color: white; color: black; border: 2px solid #FF0000;} ', "utf-8"))
             s.wfile.write(bytes('.button2:hover {background-color: #FF0000; color: white;} ', "utf-8"))
 
-            s.wfile.write(bytes('.button3 {background-color: white; color: black; border: 2px solid #000000;} ', "utf-8"))
+            s.wfile.write(bytes('.button3 {background-color: white; color: black; border: 2px solid #000000; } ', "utf-8"))
             s.wfile.write(bytes('.button3:hover {background-color: #000000; color: white;} ', "utf-8"))
 
             s.wfile.write(bytes('.button4 {background-color: white; color: black; border: 2px solid #0000FF;} ', "utf-8"))
-            s.wfile.write(bytes('.button4:hover {background-color: #0000FF; color: white;} </style>', "utf-8"))
+            s.wfile.write(bytes('.button4:hover {background-color: #0000FF; color: white;}', "utf-8"))
+
+            s.wfile.write(bytes('#grids {margin: 4px; padding: 5px; background: #FFFFFF; min-height: 500px;}', "utf-8"))
+            s.wfile.write(bytes('.row {display: flex; flow-direction: row;}', "utf-8"))
+            s.wfile.write(bytes('.grid {background: #FFFFFF; color:#000000; border: 1px slid #000000; border-radius:3px}', "utf-8"))
+            s.wfile.write(bytes('.grid:hover {border-color: #696969; color: #FFFAF0; cursor: pointer:}</style>', "utf-8"))
+
+            s.wfile.write(bytes('<script> (function(){color_green(); color_red(); color_black(); color_blue();})', "utf-8"))
+            s.wfile.write(bytes('function generate_grid(){$("renderButton").on("click", function() {var content = "";var num = 1; for (var i = 1; i <= room_height; i++) {for (var j = 1; j <= room_length; j++) {if (j === 1) {content += "<div class="row"><div class="grid">" + num + "</div>";} else if (j === value) {content += "<div class="grid">" + num + "</div></di} else {content += "<div class="grid">" + num + "</div>";}num++;}}$("#grids");});})}', "utf-8"))
+            s.wfile.write(bytes('function color_green(){$("#grids").on("click", ".grid", function(){var = value $(this).text(); alert("You click "+ value):});}', "utf-8"))
+            s.wfile.write(bytes('</script>', "utf-8"))
+ 
 
             s.wfile.write(bytes('<button class="button button1">Click here to add an attachment point for the rails in the ceiling </button>', "utf-8"))
             s.wfile.write(bytes('<button class="button button2">Click here to add locations the cart should visit </button>', "utf-8"))
             s.wfile.write(bytes('<button class="button button3">Click here to add an obstacle in the room, the feeder can not pass through these points </button>', "utf-8"))
             s.wfile.write(bytes('<button class="button button4">Click here the feeding location for the cart </button>', "utf-8"))
 
-            s.wfile.write(bytes('<h4>Grid of the room:</h4>', "utf-8"))
+            s.wfile.write(bytes('<div id="grids"></div>', "utf-8"))
+
+            """s.wfile.write(bytes('<h4>Grid of the room:</h4>', "utf-8"))
             s.wfile.write(bytes('<head><style> table {width:50%;} table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;} </style></head>', "utf-8"))
-            
-            s.wfile.write(bytes('<style>.buttonCell {border: none; color: white; padding: 10px 10px; transition-duration: 0.4s; cursor: pointer;}', "utf-8"))
             table = ""
             cols = ""
-
-            # Lage knapper i alle cellene, slik at man fargelegger ved "onclick" på knappene
-            # fargen er bestemt av hvilken av de store knappene man har trykket på
-            # lar det være en verdi, slik at knappene da får en verdi og man kan hente ut den informasjonen
+            for w in range(room_width):
+                cols += "<td></td>"
 
             for l in range(room_length):
-                for w in range(room_width):
-                    cols += '<td><button name="cell"'+ str(l) + '"."' + str(w) + '" type="button" onclick="alert("Hello world!")"></button></td>' #buttonCell row.column
                 table += "<tr>" + cols + "</tr>"
-            
-            s.wfile.write(bytes('<table>' + table + '</table>', "utf-8"))
-
+            s.wfile.write(bytes('<table>' + table + '</table>', "utf-8"))"""
             s.wfile.write(bytes('<br><br><input type="submit" value="Submit"></form><p> Click "Submit" to send order.</p></body></html>', "utf-8"))
 
             return room_height, room_length, room_width
+
 
 
 if __name__ == '__main__':
--- server_grid.py
+++ server_grid.py
@@ -1,5 +1,4 @@
-from http.server 
-import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 
 room_height = 0
@@ -50,67 +49,14 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes("<body><p>Let's order a chair</p>", "utf-8"))
             s.wfile.write(bytes('</body></html>', "utf-8"))
         elif path.find("/orderRail") != -1: #The webpage path to order a chair
-            s.wfile.write(bytes('<html><body><h2>Determine rail specifications for your K2 EasyFeed:</h2>', "utf-8"))
-            s.wfile.write(bytes('<form action="/orderRail" method="post">', 'utf-8')) #Create a form to take in values
-            
-            s.wfile.write(bytes('<h4>Set room size (m):</h4>', "utf-8"))
-            s.wfile.write(bytes('<br>Room hight:<br><input type="text" name="room_hight" value="0">', "utf-8"))
-            s.wfile.write(bytes('<br>Room length:<br><input type="text" name="room_length" value="0">', "utf-8"))
-            s.wfile.write(bytes('<br>Room width:<br><input type="text" name="room_width" value="0">', "utf-8"))
-            s.wfile.write(bytes('<br><br><input type="submit" value="Render"></form><p> Click "Render" to make the preview grid</p>', "utf-8"))
-            
-            s.wfile.write(bytes('<h4>Add variales in the room:</h4>', "utf-8"))
-
-            s.wfile.write(bytes('<style>.button {border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 1px 2px; transition-duration: 0.4s; cursor: pointer;}', "utf-8"))
-            s.wfile.write(bytes('.button1 {background-color: white; color: black; border: 2px solid #008000;} ', "utf-8"))
-            s.wfile.write(bytes('.button1:hover {background-color: #008000; color: white;} ', "utf-8"))
-           
-
-            s.wfile.write(bytes('.button2 {background-color: white; color: black; border: 2px solid #FF0000;} ', "utf-8"))
-            s.wfile.write(bytes('.button2:hover {background-color: #FF0000; color: white;} ', "utf-8"))
-
-            s.wfile.write(bytes('.button3 {background-color: white; color: black; border: 2px solid #000000; } ', "utf-8"))
-            s.wfile.write(bytes('.button3:hover {background-color: #000000; color: white;} ', "utf-8"))
-
-            s.wfile.write(bytes('.button4 {background-color: white; color: black; border: 2px solid #0000FF;} ', "utf-8"))
-            s.wfile.write(bytes('.button4:hover {background-color: #0000FF; color: white;}', "utf-8"))
-
-            s.wfile.write(bytes('#grids {margin: 4px; padding: 5px; background: #FFFFFF; min-height: 500px;}', "utf-8"))
-            s.wfile.write(bytes('.row {display: flex; flow-direction: row;}', "utf-8"))
-            s.wfile.write(bytes('.grid {background: #FFFFFF; color:#000000; border: 1px slid #000000; border-radius:3px}', "utf-8"))
-            s.wfile.write(bytes('.grid:hover {border-color: #696969; color: #FFFAF0; cursor: pointer:}</style>', "utf-8"))
-
-            s.wfile.write(bytes('<script> (function(){color_green(); color_red(); color_black(); color_blue();})', "utf-8"))
-            s.wfile.write(bytes('function color_green(){$("#grids").on("click", ".grid", function(){var}}', "utf-8"))
-            s.wfile.write(bytes('', "utf-8"))
-            s.wfile.write(bytes('', "utf-8"))
-
-            s.wfile.write(bytes('<button class="button button1">Click here to add an attachment point for the rails in the ceiling </button>', "utf-8"))
-            s.wfile.write(bytes('<button class="button button2">Click here to add locations the cart should visit </button>', "utf-8"))
-            s.wfile.write(bytes('<button class="button button3">Click here to add an obstacle in the room, the feeder can not pass through these points </button>', "utf-8"))
-            s.wfile.write(bytes('<button class="button button4">Click here the feeding location for the cart </button>', "utf-8"))
-
-            s.wfile.write(bytes('', "utf-8"))
-            s.wfile.write(bytes('', "utf-8"))
-            s.wfile.write(bytes('', "utf-8"))
-            s.wfile.write(bytes('', "utf-8"))
-
-            s.wfile.write(bytes('<h4>Grid of the room:</h4>', "utf-8"))
-            s.wfile.write(bytes('<head><style> table {width:50%;} table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;} </style></head>', "utf-8"))
-            table = ""
-            cols = ""
-            for w in range(room_width):
-                cols += "<td></td>"
-
-            for l in range(room_length):
-                table += "<tr>" + cols + "</tr>"
-            s.wfile.write(bytes('<table>' + table + '</table>', "utf-8"))
-            s.wfile.write(bytes('<br><br><input type="submit" value="Submit"></form><p> Click "Submit" to send order.</p></body></html>', "utf-8"))
-        else:
+            s.path = '//Users//eier//Documents//GitHub//TMM4275-Assignment2//railServer.html'
             s.wfile.write(
                 bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
             s.wfile.write(
-                bytes("<body><p>The path: " + path + "</p>", "utf-8"))
+                bytes("<body><p>Current path: " + path + "</p>", "utf-8"))
+        else:
+            s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
+            s.wfile.write(bytes("<body><p>The path: " + path + "</p>", "utf-8"))
             s.wfile.write(bytes('</body></html>', "utf-8"))
 
     def do_POST(s):
@@ -147,29 +93,43 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(bytes('<br>Room hight:<br><input type="text" name="room_height" value="' + str(room_height) + '">', "utf-8"))
             s.wfile.write(bytes('<br>Room length:<br><input type="text" name="room_length" value="' + str(room_length) + '">', "utf-8"))
             s.wfile.write(bytes('<br>Room width:<br><input type="text" name="room_width" value="' + str(room_width) + '">', "utf-8"))
-            s.wfile.write(bytes('<br><br><input type="submit" value="Render"></form><p> Click "Render" to make the preview grid</p>', "utf-8"))
-            
+            s.wfile.write(bytes('<br><br> <button id="renderButton"> Render </button> </form><p> Click "Render" to make the preview grid</p>', "utf-8"))
+
             s.wfile.write(bytes('<h4>Add variales in the room:</h4>', "utf-8"))
             
             s.wfile.write(bytes('<style>.button {border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 1px 2px; transition-duration: 0.4s; cursor: pointer;}', "utf-8"))
             s.wfile.write(bytes('.button1 {background-color: white; color: black; border: 2px solid #008000;} ', "utf-8"))
             s.wfile.write(bytes('.button1:hover {background-color: #008000; color: white;} ', "utf-8"))
+           
 
             s.wfile.write(bytes('.button2 {background-color: white; color: black; border: 2px solid #FF0000;} ', "utf-8"))
             s.wfile.write(bytes('.button2:hover {background-color: #FF0000; color: white;} ', "utf-8"))
 
-            s.wfile.write(bytes('.button3 {background-color: white; color: black; border: 2px solid #000000;} ', "utf-8"))
+            s.wfile.write(bytes('.button3 {background-color: white; color: black; border: 2px solid #000000; } ', "utf-8"))
             s.wfile.write(bytes('.button3:hover {background-color: #000000; color: white;} ', "utf-8"))
 
             s.wfile.write(bytes('.button4 {background-color: white; color: black; border: 2px solid #0000FF;} ', "utf-8"))
-            s.wfile.write(bytes('.button4:hover {background-color: #0000FF; color: white;} </style>', "utf-8"))
+            s.wfile.write(bytes('.button4:hover {background-color: #0000FF; color: white;}', "utf-8"))
+
+            s.wfile.write(bytes('#grids {margin: 4px; padding: 5px; background: #FFFFFF; min-height: 500px;}', "utf-8"))
+            s.wfile.write(bytes('.row {display: flex; flow-direction: row;}', "utf-8"))
+            s.wfile.write(bytes('.grid {background: #FFFFFF; color:#000000; border: 1px slid #000000; border-radius:3px}', "utf-8"))
+            s.wfile.write(bytes('.grid:hover {border-color: #696969; color: #FFFAF0; cursor: pointer:}</style>', "utf-8"))
+
+            s.wfile.write(bytes('<script> (function(){color_green(); color_red(); color_black(); color_blue();})', "utf-8"))
+            s.wfile.write(bytes('function generate_grid(){$("renderButton").on("click", function() {var content = "";var num = 1; for (var i = 1; i <= room_height; i++) {for (var j = 1; j <= room_length; j++) {if (j === 1) {content += "<div class="row"><div class="grid">" + num + "</div>";} else if (j === value) {content += "<div class="grid">" + num + "</div></di} else {content += "<div class="grid">" + num + "</div>";}num++;}}$("#grids");});})}', "utf-8"))
+            s.wfile.write(bytes('function color_green(){$("#grids").on("click", ".grid", function(){var = value $(this).text(); alert("You click "+ value):});}', "utf-8"))
+            s.wfile.write(bytes('</script>', "utf-8"))
+ 
 
             s.wfile.write(bytes('<button class="button button1">Click here to add an attachment point for the rails in the ceiling </button>', "utf-8"))
             s.wfile.write(bytes('<button class="button button2">Click here to add locations the cart should visit </button>', "utf-8"))
             s.wfile.write(bytes('<button class="button button3">Click here to add an obstacle in the room, the feeder can not pass through these points </button>', "utf-8"))
             s.wfile.write(bytes('<button class="button button4">Click here the feeding location for the cart </button>', "utf-8"))
 
-            s.wfile.write(bytes('<h4>Grid of the room:</h4>', "utf-8"))
+            s.wfile.write(bytes('<div id="grids"></div>', "utf-8"))
+
+            """s.wfile.write(bytes('<h4>Grid of the room:</h4>', "utf-8"))
             s.wfile.write(bytes('<head><style> table {width:50%;} table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;} </style></head>', "utf-8"))
             table = ""
             cols = ""
@@ -178,9 +138,7 @@ class MyHandler(BaseHTTPRequestHandler):
 
             for l in range(room_length):
                 table += "<tr>" + cols + "</tr>"
-            
-            s.wfile.write(bytes('<table>' + table + '</table>', "utf-8"))
-
+            s.wfile.write(bytes('<table>' + table + '</table>', "utf-8"))"""
             s.wfile.write(bytes('<br><br><input type="submit" value="Submit"></form><p> Click "Submit" to send order.</p></body></html>', "utf-8"))
 
             return room_height, room_length, room_width
