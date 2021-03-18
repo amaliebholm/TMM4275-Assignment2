97252aa0222eef3178f18a5cde096d198e75c9e8
Magnus Myklegard <mykle@dhcp-10-220-222-42.wlan.ntnu.no>
2021-03-18 15:10:07 +0100

    Adjusting the astar algorithm

--- algorithm.py
+++ algorithm.py
@@ -31,6 +31,42 @@ def astar(input_matrix):
 
     visited = {}  # empty dictionary of nodes who have been visited
 
+    while True:
+        # first state
+        current_state = fringe.pop(0)
+
+        # checking if we are in the goal position:
+        (i, j) = current_state[0]
+        if input_matrix[i][j] == 3:
+            path = [current_state[0]] + current_state[1]
+            path.reverse()
+            return path
+        # Setting the cost
+        visited[(i, j)] = current_state[2]
+
+        # check for if the path has a viable turn, (has to be able to turn with a radius of 2m). The neighbor nodes needs to form a freespace kvadrat for a turn.
+        # exploring neighbors:
+        neighbors = list()
+        if i > 0 and input_matrix[i-1][j] > 0:
+            neighbors.append((i-1, j))
+        if i < height and input_matrix[i+1][j] > 0:
+            neighbors.append((i+1, j))
+        if j > 0 and input_matrix[i][j-1] > 0:
+            neighbors.append((i, j-1))
+        if j < width and input_matrix[i][j+1] > 0:
+            neighbors.append((i, j+1))
+        print("Current state: ", current_state)
+        for n in neighbors:
+            next_cost = current_state[2] + 1
+            if n in visited and visited[n] >= next_cost:
+                continue
+            fringe.append((n, [current_state[0]] + current_state[1],
+                           next_cost, heuristic(n[0], n[1])))
+
+        # resort the list (SHOULD use a priority queue here to avoid re-sorting all the time)
+        fringe.sort(key=comp)
+        print("Sorted path: ", fringe)
+
     return path
 
 
