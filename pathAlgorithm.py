fef547e91204ad45f9c5283eeaf2b8a4cbfab8cf
Magnus Myklegard <mykle@Magnuss-MacBook-Pro.local>
2021-03-25 16:55:09 +0100

    made turns available

--- pathAlgorithm.py
+++ pathAlgorithm.py
@@ -1,11 +1,13 @@
 from scipy.spatial.distance import cdist
 import numpy as np
+import numpy
 import math
 import sys
 import matplotlib.pyplot as plt
 # math library made for this algorithmf
 from calculations import Calculations as calc
 from scipy.spatial import distance
+import sympy
 
 
 # The path algorithm takes in a list of attatchment points where the first point is the location of the feeder.
@@ -48,25 +50,25 @@ class pathAlgorithm:
         dummy_list = []
         for i in updated_data:
             dummy_list.append(i)
-        print("DUMMY: ", dummy_list)
+        # print("DUMMY: ", dummy_list)
 
         for data_point in enumerate(updated_data):
 
             if data_point is not None:
                 current_point = best_path[-1]
-                print("CURRENT INDEX:  ", current_point)
-                print("BEFORE REMOVING: ", updated_data)
+                # print("CURRENT INDEX:  ", current_point)
+                # print("BEFORE REMOVING: ", updated_data)
 
                 if current_point in updated_data:
                     dummy_list.remove(current_point)
-                    print("AFTER REMOVING: ", dummy_list)
+                    # print("AFTER REMOVING: ", dummy_list)
 
                 # finds the closest point.
                 if len(dummy_list) != 0:
                     point = self.closest_node(current_point, dummy_list)
                     if point not in best_path:
                         best_path.append(point)
-                        print("CLOSEST AND ADDED POINT: ", point)
+                        # print("CLOSEST AND ADDED POINT: ", point)
                 else:
                     pass
             else:
@@ -75,7 +77,7 @@ class pathAlgorithm:
         # next_position = path[index]
             print("PATH: ", best_path)
 
-        return best_path
+        return self.railAlgorithm(best_path)
 
    # Function for finding the center of the fictive circle given the current position, the next and the direction.
     def findCenter(self, current_position, next_position, direction):
@@ -108,11 +110,15 @@ class pathAlgorithm:
 
         normalizedVector = [point[0]/distance, point[1]/distance]
 
+        print("Normalized Vector: ", normalizedVector)
+
         return normalizedVector
 
     def directonalVector(self, point1, point2):
 
         dirVec = [point1[0]-point2[0], point1[1] - point2[1]]
+
+        print("Direction Vector: ", dirVec)
 
         return dirVec
 
@@ -124,15 +130,20 @@ class pathAlgorithm:
         unit_vector2 = np.divide(vector2, np.linalg.norm(vector2))
 
         dot_product = np.dot(unit_vector1, unit_vector2)
-        psi = np.arccos(dot_product)
+        psi = numpy.arccos(dot_product)
+        print("PSI: ", psi)
 
         return psi
 
     # Function for finding the arc angle
     def arcInclusiveAngle(self, angleVectors):
         psi = angleVectors
-        alpha = 2*np.arcsin((psi/2))
-
+        psi = sympy.cot((psi/2))
+        print("PSI/2: ", psi)
+        psi = math.radians(psi)
+        alpha = numpy.arcsin(psi)
+        alpha = 2*alpha
+        print("ALPHA: ", alpha)
         return alpha
 
     def InLineOfSight(self, point1, point2, point3):
@@ -152,242 +163,97 @@ class pathAlgorithm:
         else:
             return direction
 
-    # Function for getting the intercepting tangent on the arc
+    def railAlgorithm(self, best_path):
+        index = 0
+        curved_path = []
 
-    def getTangentPointM(self, center, point3, point2, direction):
+        # initialize
+        '''
+        vector = [best_path[0], best_path[1]]
+        curved_path.append(vector)
+        '''
+        print("THE BEST PATH: ", best_path)
+        print("THE curved path: ", curved_path)
+        '''
+        current_position = curved_path[index][0]
+        next_position = curved_path[index][1]
+        '''
 
-        distance = self.getDistance(center, point3)
-        print(distance)
-
-        angle_point3_center_M = math.acos(self.turn_radius / distance)
-
-        N = [center[0], point3[1]]
-        angle_point3_center_N = math.asin(
-            (point3[0] - N[0]) / distance)  # arc sine
-
-        if angle_point3_center_N < 0:
-            angle_point3_center_N *= -1
-        angleRotation = 0
-
-        # Mapping which qaudrant N is located in
-        # I quadrant:
-        if (point3[0] - center[0] > 0) and (point3[1] - center[1] > 0):
-            if direction > 0:
-                angleRotation = -math.pi / 2 + angle_point3_center_M + angle_point3_center_N
-                angleRotation *= -1
-            else:
-                angleRotation = math.pi / 2 + angle_point3_center_M - angle_point3_center_N
-
-        # II quadrant:
-        if (point3[0] - center[0] < 0) and (point3[1] - center[1] > 0):
-            if direction < 0:
-                angleRotation = math.pi / 2 + angle_point3_center_M + angle_point3_center_N
-            else:
-                angleRotation = math.pi / 2 - \
-                    (angle_point3_center_M - angle_point3_center_N)
-
-        # III quadrant:
-        if (point3[0] - center[0] < 0) and (point3[1] - center[1] < 0):
-            if direction > 0:
-                angleRotation = 1.5 * math.pi - \
-                    (angle_point3_center_M + angle_point3_center_N)
-            else:
-                angleRotation = 1.5 * math.pi + \
-                    (angle_point3_center_M - angle_point3_center_N)
-
-        # IV quadrant:
-        if (point3[0] - center[0] > 0) and (point3[1] - center[1] < 0):
-            if direction > 0:
-                angleRotation = math.pi / 2 + \
-                    (angle_point3_center_M - angle_point3_center_N)
-                angleRotation *= -1
-            else:
-                angleRotation = -math.pi / 2 + angle_point3_center_M + angle_point3_center_N
-
-        # Getting the coordinates of M:
-        M = self.pointOnArc(center, angleRotation)
-
-        return M
-
-    # function for fetching a point on a arc
-    def pointOnArc(self, center, angle):
-
-        x_shifted = self.turn_radius * math.cos(angle)
-        y_shifted = self.turn_radius * math.sin(angle)
-
-        arcPoint = [center[0] + x_shifted, center[1] + y_shifted]
-        print("The point on arc: ", arcPoint)
-        return arcPoint
-
-    def getStartEndAng(self, center, point1, point2):
-        # Find the first point - point1 or point2 - w.r.t. the quaters of the circle for given center.
-        angle_1 = self.angleBetween(center, (center[0] + 5, center[1]), point1)
-        angle_2 = self.angleBetween(center, (center[0] + 5, center[1]), point2)
-        # Check if the point is below the center --> two arcs to be generated (to be processed at the caller side).
-
-        # IV quadrant - incoming
-        if center[1] > point1[1] and center[0] < point1[0]:
-            angle_1 *= -1
-        # IV quadrant - outgoing
-        if center[1] > point2[1] and center[0] < point2[0]:
-            angle_2 *= -1
-
-        # III quadrant - incoming
-        if center[1] > point1[1] and center[0] > point1[0]:
-            angle_1 = 2 * math.pi - angle_1
-        # III quadrant - outgoing
-        if center[1] > point2[1] and center[0] > point2[0]:
-            angle_2 = 2 * math.pi - angle_2
-
-        if angle_1 < angle_2:
-            return (angle_1, angle_2)
+        if len(curved_path) > 2:
+            previous_position = curved_path[index - 1]
         else:
-            return (angle_2, angle_1)
+            pass
 
-    def angleBetween(self, center, point1, point2):
-        vector1 = (point1[0] - center[0], point1[1] - center[1])
-        vector2 = (point2[0] - center[0], point2[1] - center[1])
+        while index < len(best_path)-1:
+            index += 1
+            '''
+            current_position = curved_path[index][0]
+            next_position = curved_path[index][1]
+            '''
+            print("ITERATION: ", index)
+            print(best_path[index-1])
+            print(best_path[index])
+            print(best_path[index+1])
 
-        scalar = vector1[0] * vector2[0] + vector1[1] * vector2[1]
-
-        length_vector1 = math.sqrt(vector1[0] ** 2 + vector1[1] ** 2)
-        length_vector2 = math.sqrt(vector2[0] ** 2 + vector2[1] ** 2)
-
-        angle = math.acos(scalar / (length_vector1 * length_vector2))
-
-        return angle
-
-    def algorithm(self, attatchment_locations):
-        path = []
-        tracker = 0
-        # z- direction, is always zero since it is the roof stags that are scaled while the rail stays leveled.
-        z = 0
-
-        # not correct as locations are added at the end
-        # all_locations = attatchment_locations + desired_locations
-
-        # Main loop of the algorithm, runs until the whole list of attatchment points have been initilized.
-
-        while tracker < len(attatchment_locations)-1:
-
-            # initilizing the first line
-            # the first and second attatchment points
-            # Keeping track of previous, current and next position
-            current_position = attatchment_locations[tracker]
-            next_position = attatchment_locations[tracker + 1]
-            previous_position = attatchment_locations[tracker - 1]
-            tracker += 1
-
-            vector = [current_position, next_position]
-            path.append(vector)
-            print("Path: ", path[-1])
-
-            # Finding the best path
-
-            # checking for turn and what direction the turn is
-            direction = self.InLineOfSight(
-                path[-1][0], path[-1][1], current_position)
-
-            # On the same line
-            if len(path[-1]) == 2 and direction != 0:
-                vector = [current_position, next_position]
-                path.append(vector)
-
-            # if direction == 1 and path[-1][0][1] > path[-1][1][1]:
+            if index == 1:
+                vector = [best_path[index-1], best_path[index]]
+                curved_path.append(vector)
 
             else:
+                direction = self.InLineOfSight(
+                    curved_path[-1][0], curved_path[-1][1], best_path[index-1])
+                print("DIRECTION: ", direction)
+                # Check for turn
+                # no turn --> Straight line
+                if (len(curved_path[-1]) == 2) and direction == 0:
+                    vector = [best_path[index-1], best_path[index]]
+                    curved_path.append(vector)
 
-                # initializing necessery helper functions:
+                else:  # Turn
+                    '''
+                    vector = [best_path[index], best_path[index+1]]
+                    curved_path.append(vector)
+                    '''
+                    print(curved_path[-1][0])
+                    print(curved_path[-1][1])
+                    print(best_path[index-1])
 
-                center = self.findCenter(
-                    path[-1][0], path[-1][1], direction)
+                    if len(curved_path[-1]) == 2:
 
-                psi = self.angleBetweenVectors(path[-1][0], path[-1][1])
+                        direction = self.InLineOfSight(
+                            curved_path[-1][0], curved_path[-1][1], best_path[index - 1])
 
-                # The angle between the center and  the  two endpoints of the arc
-                arcInteriorAngle = self.arcInclusiveAngle(psi)
+                        center = self.findCenter(
+                            curved_path[-1][0], curved_path[-1][1], direction)
 
-                dirVec = self.directonalVector(path[-1][0], path[-1][1])
+                        psi = self.angleBetweenVectors(
+                            curved_path[-1][1], best_path[index-1])
 
-                PointOnArc = self.getTangentPointM(
-                    center, current_position, path[-1][1], direction)
+                        # The angle between the center and  the  two endpoints of the arc
+                        arcInteriorAngle = self.arcInclusiveAngle(psi)
 
-                startEndAngles = self.getStartEndAng(
-                    center, path[-1][1], PointOnArc)
+                        directionVector = self.directonalVector(
+                            best_path[index], best_path[index-1])
 
-                if (direction == 1 and path[-1][0][1] > path[-1][1][1]):
+                     # Calculating distance along one of the vectors to the interception point on the arc. (End of arc)
 
-                    if (startEndAngles[0] < 0):  # Outgoing from IV quater
-                        # newEndAng = 2 * math.pi + startEndAngles[0]
-                        # newStartAng = startEndAngles[1]
-                        pointOnCurve = [path[-1][1], PointOnArc]
-                        print("POSITIONS: ", path[-1][0], path[-1][1])
-                        print("POINT ON CURVE: ", pointOnCurve)
-                        path.append(pointOnCurve)
+                        distance_to_point_end_of_arc = self.turn_radius * \
+                            sympy.cot(psi/2)
 
-                    elif center[1] > current_position[1]:
+                        print("Distance to end: ", distance_to_point_end_of_arc)
 
-                        curve = [path[-1][1], PointOnArc]
-                        path.append(curve)
+                        my_new_list = [
+                            i * distance_to_point_end_of_arc for i in directionVector]
 
+                        end_point_of_arc = best_path[index-1] + my_new_list
+
+                        print("END POINT ON CURVE: ", end_point_of_arc)
                     else:
-                        newStartAng = startEndAngles[1]
-                        newEndAng = startEndAngles[0]
+                        pass
 
-                        pointOnCurve = [path[-1][1], PointOnArc]
-                        print("POINT ON CURVE: ", pointOnCurve)
-                        path.append(pointOnCurve)
+        return curved_path
 
-                    # Incoming line to IV quater
-                elif (direction > 0) and (path[-1][0][1] < path[-1][1][1]):
-                    # redifine the angles first
-                    newStartAng = 0
-                    newEndAng = 0
-                    # Outgoing from IV quater
-                    if ((startEndAngles[0] < 0) and (startEndAngles[1] < 0)):
-                        # Normal order, but in +scale
-                        newStartAng = 2 * math.pi + startEndAngles[0]
-                        newEndAng = 2 * math.pi + startEndAngles[1]
-
-                        pointOnCurve = [path[-1][1], PointOnArc]
-                        print("POINT ON CURVE: ", pointOnCurve)
-                        path.append(pointOnCurve)
-                    # Not only the IV quater, but other quater(s) as well --> aux. curve needed
-                    elif startEndAngles[0] < 0:
-                        newStartAng = startEndAngles[0] + 2 * math.pi
-                        newEndAng = startEndAngles[1]
-
-                        pointOnCurve = [path[-1][1], PointOnArc]
-                        print("POINT ON CURVE: ", pointOnCurve)
-                        path.append(pointOnCurve)
-
-                # Incoming line to I quater, but clockwise rotation
-                elif (direction < 0) and (path[-1][0][1] > path[-1][1][1]):
-                    if startEndAngles[0] < 0:
-
-                        pointOnCurve = [path[-1][1], PointOnArc]
-                        path.append(pointOnCurve)
-                        print("POINT ON CURVE: ", pointOnCurve)
-                    else:
-
-                        pointOnCurve = [path[-1][1], PointOnArc]
-                        path.append(pointOnCurve)
-                        print("POINT ON CURVE: ", pointOnCurve)
-
-                elif (startEndAngles[0] < 0):
-
-                    pointOnCurve = [path[-1][1], PointOnArc]
-                    path.append(pointOnCurve)
-
-                else:
-
-                    pointOnCurve = [path[-1][1], PointOnArc]
-                    path.append(pointOnCurve)
-                    # Add the line (PointOnArc, the point)
-                    vector = [PointOnArc, attatchment_locations[tracker]]
-                    path.append(vector)
-
-        return path
+    # Function for getting the intercepting tangent on the arc
 
 
 PathAlgorithm = pathAlgorithm(2000, [0, 0], [-2000, -2000])
@@ -396,9 +262,10 @@ data = [[0, 0], [0, 1700], [2500, 4000], [3500, -4000],
         [10000, 6000], [-1000, 5900], [-2000, -2000]]
 
 # desired_locations = [(500, 500), (2000, 3000), (2700, 4000)]
-
-path = PathAlgorithm.algorithm(data)
+obsticles = []
+path = PathAlgorithm.bestPath(data, obsticles)
 print(path)
+
 
 '''
 path = [[[0, 0, 0], [0, 1700, 0]], [[2500, 4000, 0], [
@@ -410,7 +277,7 @@ y = []
 
 pointlist = [[0, 0], [0, 10000], [-2000, 12000], [-10000, 12000],
              [-11414.21356, 11414.21356], [-13414.21356, 9414.21356]]
-
+'''
 for i in path:
     x.append(i[0])
     print(x)
@@ -423,5 +290,5 @@ print(y)
 plt.scatter(x, y)
 plt.plot(x, y)
 plt.show()
-
+'''
 # path = [[0, 0, 0], [100, 0, 0], [100, 400, 0], [100, 400, 0], [0, 400, 0]]
